114ae2ab09e05669c0647929ea1f1ac58759573f
Andrew Borodin <aborodin@vmail.ru>
2012-09-28 11:05:23 +0400

    Rename default callbacks of widget and dialog.
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- src/diffviewer/ydiff.c
+++ src/diffviewer/ydiff.c
@@ -3352,7 +3352,7 @@ dview_callback (Widget * w, Widget * sender, widget_msg_t msg, int parm, void *d
         return MSG_HANDLED;
 
     default:
-        return default_widget_callback (sender, msg, parm, data);
+        return widget_default_callback (sender, msg, parm, data);
     }
 }
 
@@ -3409,7 +3409,7 @@ dview_dialog_callback (Dlg_head * h, Widget * sender, dlg_msg_t msg, int parm, v
         return MSG_HANDLED;
 
     default:
-        return default_dlg_callback (h, sender, msg, parm, data);
+        return dlg_default_callback (h, sender, msg, parm, data);
     }
 }
 
