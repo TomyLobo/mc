3b621114db4bdc7e7ffaedd225b98fc460b6636c
Andrew Borodin <aborodin@vmail.ru>
2012-09-28 11:41:04 +0400

    Remove DLG_WANT_IDLE. Use W_WANT_IDLE instead.
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

--- src/filemanager/midnight.c
+++ src/filemanager/midnight.c
@@ -1428,7 +1428,7 @@ midnight_callback (WDialog * h, Widget * sender, dlg_msg_t msg, int parm, void *
 
     case DLG_IDLE:
         /* We only need the first idle event to show user menu after start */
-        set_idle_proc (h, 0);
+        widget_want_idle (WIDGET (h), FALSE);
 
         if (boot_current_is_left)
             dlg_select_widget (get_panel_widget (0));
@@ -1757,10 +1757,14 @@ do_nc (void)
 #endif
 
     midnight_dlg = create_dlg (FALSE, 0, 0, LINES, COLS, midnight_colors, midnight_callback,
-                               midnight_event, "[main]", NULL, DLG_WANT_IDLE);
+                               midnight_event, "[main]", NULL, DLG_NONE);
 
     if (mc_global.mc_run_mode == MC_RUN_FULL)
+    {
+        /* We only need the first idle event to show user menu after start */
+        widget_want_idle (WIDGET (midnight_dlg), TRUE);
         setup_mc ();
+    }
     else
         setup_dummy_mc ();
 
